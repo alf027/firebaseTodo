2ea62ca60c8e6ba67a7568cdf835841cc41895e3
AkyunaAkish <akyunaakish@gmail.com>
2015-08-26 20:49:35 -0600

    almost done, but have an error on the edit

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -11,6 +11,20 @@ app.controller('TodosController', function($scope, $firebaseArray){
     })
   }
 
+$scope.showTodo = function(todo){
+  $scope.text = todo.text;
+  $scope.id = todo.$id;
+}
+
+$scope.saveChanges = function(){
+  var id = $scope.id;
+  var record = $scope.todos.$getRecord(id);
+  record.text = $scope.text;
+  record.completed = $scope.completed;
+
+  $scope.todo.$save(record);
+}
+
   $scope.removeTodo = function(todo){
     $scope.todos.$remove(todo);
   }
